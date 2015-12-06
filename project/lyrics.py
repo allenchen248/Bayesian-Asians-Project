19bd6054faa89c46d0161c35b36eb77bfd200fdc
Allen Chen <allenchen248@gmail.com>
2015-12-05 22:15:28 -0500

    Added in second lyrics website.

--- project/lyrics.py
+++ project/lyrics.py
@@ -1,12 +1,18 @@
 import numpy as np
 import urllib2
 import ast
+import re
 import requests
+import time
+
+from httplib import BadStatusLine
 
 # Song.resp['foreign_ids'] is an array of dicts that are key:'foreign_id', value: 'musixmatch-WW:song:3957'
 # if 3957 is the ID of the song in musixmatch.
 
 # API Key: 3476eaa823bbdd8fc9b8fe89ee98c387
+
+SLEEP_BASE = 1
 
 LYRICS_BASE = 'http://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=3476eaa823bbdd8fc9b8fe89ee98c387'
 
@@ -27,15 +33,22 @@ u = d['url']
 
 test = requests.get(u).text
 
-def grab_lyrics(params=['hello', 'adele'], urlbase='http://search.azlyrics.com/search.php?q='):
+def grab_lyrics(params=['hello', 'adele'], urlbase='http://search.azlyrics.com/search.php?q=', throw_except=False):
 	# Set up URL
 	url = urlbase
 	for p in params:
 		url += p+"+"
 	url = url[:-1]
+	print url
 
 	# Read and split into correct pieces
-	r = urllib2.urlopen(url).read()
+	try:
+		r = urllib2.urlopen(url).read()
+	except BadStatusLine:
+		if throw_except:
+			raise ValueError("Bad URL!")
+		return []
+
 	tl = r.split("text-left visitedlyr")[1:]
 
 	# Grab hyperlinks
@@ -44,8 +57,26 @@ def grab_lyrics(params=['hello', 'adele'], urlbase='http://search.azlyrics.com/s
 	# Process into lyrics
 	output = []
 	for ur in hrefs:
-		resp = urllib2.urlopen(ur).read()
-		output.append(re.sub(r'\<.*?\>', '', resp.split('cf_text_top')[1][2:]).split('Android')[0][-8].strip())
-	
-	
+		try:
+			resp = urllib2.urlopen(ur).read()
+			output.append(re.sub(r'\<.*?\>', '', resp.split('cf_text_top')[1][2:]).split('Android')[0][-8].strip())
+		except BadStatusLine:
+			if throw_except:
+				raise ValueError("Bad URL!")
+		time.sleep(SLEEP_BASE)
+
 	return output
+
+urllib2.urlopen("http://www.lyrics.com/the-hills-lyrics-the-weeknd.html")
+
+r = urllib2.urlopen("http://www.lyrics.com/earned-it-fifty-shades-of-grey-lyrics-the-weeknd.html")
+
+t = r.read()
+
+t.split("lyric_space")[1].split("itemprop=")[1][14:].split("class=\"PRINTONLY\"")[0][:-42]
+
+r = urllib2.urlopen("http://www.lyrics.com/search.php?keyword=the+weekend+the+hills&what=all&search_btn=Search")
+
+t = r.read()
+
+t.split("rightcontent")[1].split("bottom_wrapper")[0].split("href")
